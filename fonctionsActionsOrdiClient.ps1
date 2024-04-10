d38e932a7b491515535d68203dbf2be986bad632
JuGuillot <161329881+JuGuillot@users.noreply.github.com>
2024-04-10 15:52:12 +0200

    Update fonctionsActionsOrdiClient.ps1
    ajout des fonctions action utilisateur

--- fonctionsActionsOrdiClient.ps1
+++ fonctionsActionsOrdiClient.ps1
@@ -53,91 +53,129 @@ function fonction_menu_action_utilisateur {
     }
 }
 
-#Création de compte utilisateur local
-#function Creation_compte_utilisateur_local {
-#    $newUser = Read-Host "Merci de renseigner un nom de compte à créer :"
- #   $userExists = Get-LocalUser -Name $newUser -ErrorAction SilentlyContinue
-
-  #  if ($userExists) {
-   #     Write-Host "Utilisateur $newUser existe. Sortie du script."
- #   }
-    #else {
-     #   New-LocalUser -Name $newUser
-      #  Write-Host "Utilisateur $newUser créé !"
-  #  }
-#}
-
-#Changement de mot de passe
-function Changement_de_mot_de_passe {
-    $account = Read-Host "Merci de renseigner le compte dont vous voulez changer le mot de passe :"
-    $userExists = Get-LocalUser -Name $account -ErrorAction SilentlyContinue
-
-    if ($userExists) {
-        Write-Host "Merci de rentrer un nouveau mot de passe :"
-        Set-LocalUser -Name $account -ChangePasswordAtLogon $true
+# Création d'utilisateur 
+Invoke-Command -ComputerName 172.16.10.20 -ScriptBlock { function Creation_compte_utilisateur_local {
+    $answer = Read-Host "Voulez-vous creer un nouveau compte utilisateur local ? (o/n)"
+    if ($answer -eq "o") {
+        $createUser = Read-Host "Rentrez le nom de l'utilisateur a creer"
+        # Vérification que l'utilisateur existe
+        if (Get-LocalUser -Name $createUser -ErrorAction SilentlyContinue) {
+            Write-Host "L'utilisateur $createUser existe deja."
+        }
+        # Si non création de l'utilisateur
+        else {
+            New-LocalUser $createUser 
+            Write-Host "Le compte $createUser a ete cree avec succes."
+        }
     }
     else {
-        Write-Host "Le compte $account n'existe pas."
+        if ($answer -eq "n") {
+            Write-Host "Operation annulee."
+        }
     }
 }
+Creation_compte_utilisateur_local } -Credential wilder
 
-# Suppression de compte local
-function Suppression_de_compte {
-    $delUser = Read-Host "Donner un nom de compte à supprimer:"
-    $userExists = Get-LocalUser -Name $delUser -ErrorAction SilentlyContinue
 
-    if ($userExists) {
-        Remove-LocalUser -Name $delUser -Force
-        Write-Host "Utilisateur $delUser supprimé"
+# Suppresion d'utilisateur 
+Invoke-Command -ComputerName 172.16.10.20 -ScriptBlock { function Suppression_de_compte {
+    $answer = Read-Host "Voulez-vous supprimer un compte utilisateur local ? (o/n)"
+    if ($answer -eq "o") {
+        $delUser = Read-Host "Rentrez le nom de l'utilisateur a supprimer"
+        # Vérification que l'utilisateur existe
+        if (Get-LocalUser -Name $delUser -ErrorAction SilentlyContinue) {
+            # Si oui suppression de l'utilisateur
+            Remove-LocalUser $delUser 
+            Write-Host "Le compte $delUser a ete supprime avec succes."
+        }
     }
     else {
-        Write-Host "Utilisateur $delUser non-existant"
+        if ($answer -eq "n") {
+            Write-Host "Operation annulee."
     }
 }
+}
+Suppression_de_compte } -Credential wilder
 
-# Désactivation d'un compte utilisateur
-function Desactivation_compte_utilisteur {
-    $deacUser = Read-Host "Merci de renseigner un compte à désactiver :"
-    $userExists = Get-LocalUser -Name $deacUser -ErrorAction SilentlyContinue
-
-    if ($userExists) {
-        Disable-LocalUser -Name $deacUser
-        Write-Host "Désactivation du compte en cours..."
+# Changement de mdp
+Invoke-Command -ComputerName 172.16.10.20 -ScriptBlock { function Changement_de_mot_de_passe {
+    $answer = Read-Host "Voulez-vous modifier le mot de passer d'un compte utilisateur local ? (o/n)"
+    if ($answer -eq "o") {
+        $modUser = Read-Host "Rentrez le nom de l'utilisateur"
+        # Vérification que l'utilisateur existe
+        if (Get-LocalUser -Name $modUser -ErrorAction SilentlyContinue) {
+            # Si oui changement de mot de passe de l'utilisateur
+            Set-LocalUser -Name $modUser -Password (ConvertTo-SecureString -AsPlainText -Force) 
+            Write-Host "Le mot de passe de $modUser a ete modifie avec succes."
+        }
     }
     else {
-        Write-Host "Le compte $deacUser n'existe pas."
+        if ($answer -eq "n") {
+            Write-Host "Operation annulee."
     }
 }
+}
+Changement_de_mot_de_passe } -Credential wilder
 
-# Ajout d'un utilisateur à un groupe
-function Ajout_utilisateur_a_groupe {
-    $groupUser = Read-Host "Merci de renseigner un compte :"
-    $group = Read-Host "Merci de renseigner un groupe"
-    $userExists = Get-LocalUser -Name $groupUser -ErrorAction SilentlyContinue
-
-    if ($userExists) {
-        Add-LocalGroupMember -Group $group -Member $groupUser
-        Write-Host "Ajout de l'utilisateur $groupUser au groupe $group"
+# Desactivation d'un compte
+Invoke-Command -ComputerName 172.16.10.20 -ScriptBlock { function Desactivation_compte_utilisateur {
+    $answer = Read-Host "Voulez-vous désactiver un compte utilisateur local ? (o/n)"
+    if ($answer -eq "o") {
+        $deacUser = Read-Host "Rentrez le nom de l'utilisateur"
+        # Vérification que l'utilisateur existe
+        if (Get-LocalUser -Name $deacUser -ErrorAction SilentlyContinue) {
+            # Si oui suppression de l'utilisateur
+            Disable-LocalUser toto -Confirm
+            Write-Host "Le compte $deacUser a ete supprime avec succes."
+        }
     }
     else {
-        Write-Host "Le compte $groupUser n'existe pas."
+        Write-Host "Retour au menu precedent."
     }
 }
+Desactivation_compte_utilisateur } -Credential wilder
 
-# Suppresion d'un utilisateur d'un groupe
-function Supp_user_from_group {
-    $groupUser = Read-Host "Merci de renseigner un compte :"
-    $group = Read-Host "Merci de renseigner un groupe"
-    $userExists = Get-LocalUser -Name $groupUser -ErrorAction SilentlyContinue
-
-    if ($userExists) {
-        Remove-LocalGroupMember -Group $group -Member $groupUser
-        Write-Host "Suppression de l'utilisateur $groupUser du groupe $group"
+# Ajout d'un utilisateur à un groupe local
+Invoke-Command -ComputerName 172.16.10.20 -ScriptBlock { function Ajout_utilisateur_a_groupe {
+    $answer = Read-Host "Voulez-vous ajouter un utilisateur local à un groupe ? (o/n)"
+    if ($answer -eq "o") {
+        $user = Read-Host "Rentrez le nom de l'utilisateur"
+        $addGroup = Read-Host "Rentrer le nom du groupe"
+        # Vérification que l'utilisateur existe
+        if (Get-LocalUser -Name $user -ErrorAction SilentlyContinue) {
+            # Si oui suppression de l'utilisateur
+            Add-LocalGroupMember $addGroup -Member $user -Confirm
+            Write-Host "L'utilsateur $user a ete ajoute au groupe $addGroup avec succes."
+        }
     }
     else {
-        Write-Host "Le compte $groupUser n'existe pas."
+        if ($answer -eq "n") {
+            Write-Host "Operation annulee."
+            }
     }
 }
+Ajout_utilisateur_a_groupe } -Credential wilder
+
+# Suppression d'un utilisateur d'un groupe local
+Invoke-Command -ComputerName 172.16.10.20 -ScriptBlock { function Supp_user_from_group {
+    $answer = Read-Host "Voulez-vous retirer un utilisateur local à un groupe ? (o/n)"
+    if ($answer -eq "o") {
+        $user = Read-Host "Rentrez le nom de l'utilisateur"
+        $delGroup = Read-Host "Rentrer le nom du groupe"
+        # Vérification que l'utilisateur existe
+        if (Get-LocalUser -Name $user -ErrorAction SilentlyContinue) {
+            # Si oui suppression de l'utilisateur
+            Remove-LocalGroupMember $delGroup -Member $user -Confirm
+            Write-Host "L'utilsateur $user a ete retire au groupe $delGroup avec succes."
+        }
+    }
+    else {
+        if ($answer -eq "n") {
+            Write-Host "Operation annulee."
+            }
+    }
+}
+Supp_user_from_group } -Credential wilder
 
 #Menu info ordinateur
 function fonction_menu_info_ordinateur_client {
